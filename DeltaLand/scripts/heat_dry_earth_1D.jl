18c8cb9b4f85f13d574b93382e632e30a7b0cc8c
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-08-04 14:57:32 +0200

    Update script

--- DeltaLand/scripts/heat_dry_earth_1D.jl
+++ DeltaLand/scripts/heat_dry_earth_1D.jl
@@ -3,7 +3,7 @@ using DeltaLand
 import SpeedyWeather.RingGrids: FullHEALPixGrid
 
 grid = GlobalRingGrid(ExponentialSpacing(N=50), FullHEALPixGrid(12))
-initializer = FieldInitializers(temperature = (x,y,z) -> 1.0 - 0.01*z + exp(z/10)*sin(2π*z))
+initializer = FieldInitializers(temperature = (x,z) -> 1.0 - 0.01*z + exp(z/10)*sin(2π*z))
 model = SoilModel(; grid, initializer)
 sim = initialize(model)
 @time timestep!(sim)
