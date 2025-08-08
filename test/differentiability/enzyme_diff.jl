07617e63f8afad24b907f451cb531ffad05f2c0a
Max <maximilian.gelbrecht@posteo.de>
2025-08-08 17:46:16 +0200

    initial commit enzyme tests

---
+++ test/differentiability/enzyme_diff.jl
@@ -0,0 +1,15 @@
+using Terra, Enzyme 
+
+import SpeedyWeather.RingGrids
+
+grid = GlobalRingGrid(CPU(), ExponentialSpacing(N=10), RingGrids.FullHEALPixGrid(16, RingGrids.Architectures.CPU()))
+initializer = FieldInitializers(temperature = (x,z) -> -1.0 - 0.01*z + exp(z/10)*sin(2π*z/10))
+model = SoilModel(; grid, initializer)
+sim = initialize(model)
+
+reset_tendencies!(sim.state)
+
+state = sim.state
+dstate = make_zero(state)
+
+Enzyme.autodiff(set_runtime_activity(Reverse), timestep!, Const, Duplicated(state, dstate), Const(model), Const(model.time_stepping), Const(model.time_stepping.dt))
