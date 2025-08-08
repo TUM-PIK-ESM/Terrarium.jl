b8afcb38bc747ce9a1f695e2512c36d795164eb1
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-08-08 16:57:33 +0200

    Initial docs CI setup

---
+++ docs/make.jl
@@ -0,0 +1,43 @@
+using ArgParse
+using Documenter
+using Literate
+
+using Terra
+
+s = ArgParseSettings()
+@add_arg_table! s begin
+    "--local", "-l"
+       action = :store_true
+       help = "Local docs build mode"
+    "--draft", "-d"
+       action = :store_true
+       help = "Whether to build docs in draft mode, i.e. skipping execution of examples and doctests"
+end
+parsed_args = parse_args(ARGS, s)
+
+IS_LOCAL = parsed_args["local"] || parse(Bool, get(ENV, "LOCALDOCS", "false"))
+IS_DRAFT = parsed_args["draft"] || parse(Bool, get(ENV, "DRAFTDOCS", "false"))
+if haskey(ENV, "GITHUB_ACTIONS")
+    ENV["JULIA_DEBUG"] = "Documenter"
+end
+
+makedocs(
+    format = Documenter.HTML(
+        prettyurls=get(ENV, "CI", nothing)=="true",
+        ansicolor=true,
+        collapselevel=1,
+        canonical = "https://terra.github.io/TerraDocumentation/stable/",
+        size_threshold = 600_000
+    ),      # in bytes
+    sitename = "Terra.jl",
+    authors = "Brian Groenke and contributors",
+    modules = [Terra],
+    pages = [
+        "Home"=>"index.md",
+    ],
+    draft=IS_DRAFT,
+)
+
+deployconfig = Documenter.auto_detect_deploy_system()
+
+# TODO: Add deploydocs before releasing repository
