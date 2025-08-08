8f93b70b28807727fff44a9d8e2e40485d2d276e
mahabadri <maha_badri@yahoo.fr>
2025-08-08 18:22:14 +0200

    Add auto. respiration tests to veg. model tests

--- test/vegetation/vegetation_model_tests.jl
+++ test/vegetation/vegetation_model_tests.jl
@@ -1,6 +1,10 @@
 using Terra
 using Test
 
+@testset "Autotrophic respiration" begin
+    include("autotrophic_respiration_tests.jl")
+end
+
 @testset "Vegetation carbon dynamics" begin
     include("carbon_dynamics_tests.jl")
 end
