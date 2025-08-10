a20a79a8b59fa41f785f96e1342245f79a1365c8
mahabadri <maha_badri@yahoo.fr>
2025-08-10 15:18:58 +0200

    Update veg_dynamics + veg_dynamics tests

--- src/processes/vegetation/vegetation_dynamics.jl
+++ src/processes/vegetation/vegetation_dynamics.jl
@@ -10,23 +10,25 @@ Properties:
 $TYPEDFIELDS
 """
 @kwdef struct PALADYNVegetationDynamics{NF} <: AbstractVegetationDynamics
-    "Vegetation seed fraction"
+    "Vegetation seed fraction [-]"
     ν_seed::NF = 0.001
 
     "Minimum vegetation disturbance rate [1/year]"
+    # TODO this parameter is yearly, should be changed to daily for now
     γv_min::NF = 0.002 
 end
 
 variables(::PALADYNVegetationDynamics) = (
-    prognostic(:ν, XY()), # PFT fractional area coverage
+    prognostic(:ν, XY()), # PFT fractional area coverage [-]
     auxiliary(:C_veg, XY()), # Vegetation carbon pool [kgC/m²]
-    auxiliary(:LAI_b, XY()), # Balanced Leaf Area Index 
+    auxiliary(:LAI_b, XY()), # Balanced Leaf Area Index [m²/m²]
 )
 
 """
     $SIGNATURES
 
-Computes the disturbance rate`γv`.
+Computes the disturbance rate`γv`,
+Eq. 80, PALADYN (Willeit 2016).
 """
 @inline function compute_γv(veg_dynamics::PALADYNVegetationDynamics)
     # TODO add PALADYN implemetation for the disturbance rate (depends on soil moisture)
@@ -37,13 +39,39 @@ end
 """
     $SIGNATURES
 
-Computes `ν_star` which is the maximum between the current vegetation fraction `ν` and the seed fraction `ν_seed`,
+Computes `ν_star` which is the maximum between the current vegetation fraction `ν` and the seed fraction `ν_seed` [-],
 to ensure that a PFT is always seeded.
 """
 @inline function compute_ν_star(veg_dynamics::PALADYNVegetationDynamics, ν) 
     return max(ν, veg_dynamics.ν_seed)
 end
 
+"""
+    $SIGNATURES
+
+Computes the vegetation fraction tendency for a single PFT,
+Eq. 73, PALADYN (Willeit 2016).
+"""
+@inline function compute_ν_tend(
+    veg_dynamics::PALADYNVegetationDynamics, 
+    vegcarbon_dynamics::PALADYNCarbonDynamics{NF},
+    LAI_b::NF,
+    C_veg::NF,
+    ν::NF) where NF
+
+    # Compute λ_NPP
+    λ_NPP = compute_λ_NPP(vegcarbon_dynamics, LAI_b)
+
+    # Compute the disturbance rate
+    γv = compute_γv(veg_dynamics)
+
+    # Compute ν_star
+    ν_star = compute_ν_star(veg_dynamics, ν)
+
+    # Compute the vegetation fraction tendency 
+    ν_tendency =  (λ_NPP * C_veg / ν_star) * (NF(1.0) - ν) - γv * ν_star
+    return ν_tendency
+end
 
 function compute_auxiliary!(state, model, veg_dynamics::PALADYNVegetationDynamics)
     # Nothing needed here for now
@@ -58,20 +86,18 @@ end
 @kernel function compute_tendencies_kernel!(
     state,
     veg_dynamics::PALADYNVegetationDynamics{NF},
-    carbon_dynamics::PALADYNCarbonDynamics{NF}
+    vegcarbon_dynamics::PALADYNCarbonDynamics{NF}
 ) where NF
     i, j = @index(Global, NTuple)
-    
-    # Compute λ_NPP
-    λ_NPP = compute_λ_NPP(carbon_dynamics, state.LAI_b[i, j])
 
-    # Compute the disturbance rate
-    γv = compute_γv(carbon_dynamics)
-
-    # Compute ν_star
-    ν_star = compute_ν_star(veg_dynamics, state.ν[i, j])
+    # Get inputs
+    LAI_b = state.LAI_b[i, j]
+    C_veg = state.C_veg[i, j]
+    ν = state.ν[i, j]
 
     # Compute the vegetation fraction tendency
-    state.ν_tendency[i, j] = (λ_NPP * state.NPP[i, j] / state.C_veg[i, j]) * 
-                             ν_star * (NF(1.0) - state.ν[i, j]) - γv * ν_star
+    ν_tendency = compute_ν_tend(veg_dynamics, vegcarbon_dynamics, LAI_b, C_veg, ν)
+    
+    # Store result
+    state.ν_tendency[i, j] = ν_tendency
 end
