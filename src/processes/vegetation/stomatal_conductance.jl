fe1ebbc60231b8fa5777b2b55cbe16ccaebd8dbb
mahabadri <maha_badri@yahoo.fr>
2025-08-10 14:29:58 +0200

    Update stom_conductance + stom_conductance tests

--- src/processes/vegetation/stomatal_conductance.jl
+++ src/processes/vegetation/stomatal_conductance.jl
@@ -10,7 +10,7 @@ $TYPEDFIELDS
 """
 @kwdef struct MedlynStomatalConductance{NF} <: AbstractStomatalConductance
     # TODO check pysical meaning of this parameter
-    "Parameter in optimal stomatal conductance formulation (Lin et al. 2015), PFT specific"
+    "Parameter in optimal stomatal conductance formulation, Lin et al. 2015 [-], PFT specific"
     g1::NF = 2.3 # Value for Needleleaf tree PFT 
 end
 
@@ -19,14 +19,15 @@ variables(stomcond::MedlynStomatalConductance) = (
     auxiliary(:T_air, XY()), # Surface air temperature in Celsius [°C]
     auxiliary(:q_air, XY()), # Surface air specific humidity [kg/kg]
     auxiliary(:pres, XY()), # Surface pressure [Pa]
-    auxiliary(:λc, XY()), # Ratio of leaf-internal and air CO2 concentration 
+    auxiliary(:λc, XY()), # Ratio of leaf-internal and air CO2 concentration [-]
 )
 
 """
     $SIGNATURES
 
 Computes the ratio of leaf-internal and air CO2 concentration `λc`, 
-derived from the optimal stomatal conductance model (Medlyn et al. 2011).
+derived from the optimal stomatal conductance model (Medlyn et al. 2011),
+Eq. 71, PALADYN (Willeit 2016).
 """
 @inline function compute_λc(stomcond::MedlynStomatalConductance{NF}, vpd) where NF
     λc = NF(1.0) - NF(1.6) / (NF(1.0) + stomcond.g1 / sqrt(vpd * NF(1.0e-3)))
@@ -41,9 +42,17 @@ end
 @kernel function compute_auxiliary_kernel!(state, stomcond::MedlynStomatalConductance{NF}) where NF
     i, j = @index(Global, NTuple)
 
+    # Get inputs
+    T_air = state.T_air[i, j] # °C
+    q_air = state.q_air[i, j] # kg/kg
+    pres = state.pres[i, j] # Pa
+
     # Compute vpd [Pa]
-    vpd = compute_vpd(state.T_air[i, j], state.q_air[i, j], state.pres[i, j])
+    vpd = compute_vpd(T_air, q_air, pres)
 
     # Compute λc
-    state.λc[i, j] = compute_λc(stomcond, vpd)
+    λc = compute_λc(stomcond, vpd)
+
+    # Store result
+    state.λc[i, j] = λc
 end
--- test/vegetation/stomatal_conductance_tests.jl
+++ test/vegetation/stomatal_conductance_tests.jl
@@ -3,16 +3,16 @@ using Terra: compute_λc
 using Test
 
 @testset "λc test" begin
-    stom_conductance = MedlynStomatalConductance()
+    stomcond = MedlynStomatalConductance()
     # Test vpd near zero (λc should be close to 1)
     vpd = eps() 
-    λc = compute_λc(stom_conductance, vpd)
+    λc = compute_λc(stomcond, vpd)
     @test λc ≈ 1.0
 
     # Test λc should be between 0 and 1 for a realistic positive vpd
     # TODO for very high VPD λc can become negative
     vpd = 1000 # Pa
-    λc = compute_λc(stom_conductance, vpd)
+    λc = compute_λc(stomcond, vpd)
     @test 0.0 < λc < 1.0
 end
    
