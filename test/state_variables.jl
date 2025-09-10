8a8c1ba604ce0015967ada7977a6b0529e697b9d
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-09-10 20:51:29 +0200

    Wrap state variables tests in local namespace

--- test/state_variables.jl
+++ test/state_variables.jl
@@ -1,3 +1,5 @@
+module StateVariablesTests
+
 using Terrarium
 using Test
 
@@ -5,43 +7,43 @@ import Terrarium: VarDims, XY, XYZ, prognostic, auxiliary, input, namespace
 
 DEFAULT_NF = Float32
 
-@testset "State variables for mock model type" begin
+@kwdef struct SubModel{NF} <: Terrarium.AbstractModel{NF}
+    grid
+    initializer = DefaultInitializer()
+    boundary_conditions = DefaultBoundaryConditions()
+    time_stepping::Terrarium.AbstractTimeStepper{NF} = Terrarium.ForwardEuler{DEFAULT_NF}()
+end
 
-    @kwdef struct SubModel{NF} <: Terrarium.AbstractModel{NF}
-        grid
-        initializer = DefaultInitializer()
-        boundary_conditions = DefaultBoundaryConditions()
-        time_stepping::Terrarium.AbstractTimeStepper{NF} = Terrarium.ForwardEuler{DEFAULT_NF}()
-    end
+Terrarium.variables(model::SubModel) = (
+    # duplicate naming allowed in new namesapce
+    auxiliary(:auxvar2D, XY()),
+    # inputs are handled "globally" (i.e. all inputs with a given name refer to the same field)
+    input(:forcing, XY()),
+)
 
-    Terrarium.variables(model::SubModel) = (
-        # duplicate naming allowed in new namesapce
-        auxiliary(:auxvar2D, XY()),
-        # inputs are handled "globally" (i.e. all inputs with a given name refer to the same field)
-        input(:forcing, XY()),
-    )
+@kwdef struct TestModel{NF} <: Terrarium.AbstractModel{NF}
+    grid
+    submodel = SubModel(; grid)
+    initializer = DefaultInitializer()
+    boundary_conditions = DefaultBoundaryConditions()
+    time_stepping::Terrarium.AbstractTimeStepper{NF} = Terrarium.ForwardEuler{DEFAULT_NF}()
+end
 
-    @kwdef struct TestModel{NF} <: Terrarium.AbstractModel{NF}
-        grid
-        submodel = SubModel(; grid)
-        initializer = DefaultInitializer()
-        boundary_conditions = DefaultBoundaryConditions()
-        time_stepping::Terrarium.AbstractTimeStepper{NF} = Terrarium.ForwardEuler{DEFAULT_NF}()
-    end
+struct TestClosure <: Terrarium.AbstractClosureRelation end
 
-    struct TestClosure <: Terrarium.AbstractClosureRelation end
+Terrarium.getvar(::TestClosure, dims::VarDims) = auxiliary(:closurevar, dims)
 
-    Terrarium.getvar(::TestClosure, dims::VarDims) = auxiliary(:closurevar, dims)
+Terrarium.variables(model::TestModel) = (
+    prognostic(:progvar3D, XYZ()),
+    prognostic(:cprogvar3D, XYZ(), TestClosure()),
+    prognostic(:progvar2D, XY()),
+    auxiliary(:auxvar3D, XYZ()),
+    auxiliary(:auxvar2D, XY()),
+    input(:forcing, XY()),
+    namespace(:submodel, variables(model.submodel)),
+)
 
-    Terrarium.variables(model::TestModel) = (
-        prognostic(:progvar3D, XYZ()),
-        prognostic(:cprogvar3D, XYZ(), TestClosure()),
-        prognostic(:progvar2D, XY()),
-        auxiliary(:auxvar3D, XYZ()),
-        auxiliary(:auxvar2D, XY()),
-        input(:forcing, XY()),
-        namespace(:submodel, variables(model.submodel)),
-    )
+@testset "State variable initialization" begin
 
     grid = ColumnGrid(ExponentialSpacing(N=10))
     model = TestModel{DEFAULT_NF}(; grid)
@@ -66,4 +68,6 @@ DEFAULT_NF = Float32
     @test hasproperty(sim.state.submodel, :auxvar2D) && isa(sim.state.submodel.auxvar2D, Field{Center,Center,Nothing})
     # Check that input variable is identical across namespaces
     @test sim.state.forcing === sim.state.submodel.forcing
+end
+
 end
