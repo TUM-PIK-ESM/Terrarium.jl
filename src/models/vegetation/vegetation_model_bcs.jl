d154a3cd626d3466157d4792b97656500632bd3d
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-09-10 18:56:47 +0200

    Add basic impl of vegetation BCs

---
+++ src/models/vegetation/vegetation_model_bcs.jl
@@ -0,0 +1,24 @@
+@kwdef struct PrescribedSoil{NF, Grid<:AbstractLandGrid{NF}} <: AbstractBoundaryConditions
+    "Spatial discretization"
+    grid::Grid
+
+    "Prescribed soi layer thickness in meters [m]"
+    thickness::NF = 1.0
+
+    "Prescribed soil layer porosity (soil moisture at saturation)"
+    porosity::NF = 0.5
+end
+
+variables(::PrescribedSoil) = (
+    input(:soil_temperature, XY(), units=u"°C", desc="Near-surface average soil temperature [°C]"),
+    input(:soil_moisture, XY(), desc="Root zone soil moisture as volumetric fraction [m^3/m^3]"),
+    input(:soil_field_capacity, XY(), desc="Root zone field capacity as volumetric fraction [m^3/m^3]"),
+    input(:soil_wilting_point, XY(), desc="Root zone wilting point as volumetric fraction [m^3/m^3]"),
+)
+
+# Convenience constructors for common soil upper and lower boundary conditions
+VegetationBoundaryConditions(
+    grid::AbstractLandGrid;
+    top = PrescribedAtmosphere(; grid),
+    bottom = PrescribedSoil(; grid),
+) = VerticalBoundaryConditions(; top, bottom)
