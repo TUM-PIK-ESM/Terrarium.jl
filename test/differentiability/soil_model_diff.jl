e91761546cb7b183b15574ffb4b7eef383131b99
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-09-26 19:56:22 +0200

    Add more soil energy differentiability tests

--- test/differentiability/soil_model_diff.jl
+++ test/differentiability/soil_model_diff.jl
@@ -1,8 +1,17 @@
 using Terrarium, Enzyme
 using Oceananigans: Average, Field
 
+using FreezeCurves
+
 grid = ColumnGrid(CPU(), Float64, ExponentialSpacing(N=10))
-model = SoilModel(; grid)
+# initial conditions
+initializer = FieldInitializers(
+    # steady-ish state initial condition for temperature
+    temperature = (x,z) -> -1 - 0.02*z,
+    # saturated soil
+    pore_water_ice_saturation = 1.0,
+)
+model = SoilModel(; grid, initializer)
 sim = initialize(model)
 timestep!(sim)
 
@@ -20,7 +29,47 @@ end
 
 dostep!(state, model, model.time_stepping, 1.0)
 
-Enzyme.autodiff(set_strong_zero(set_runtime_activity(Reverse)), dostep!, Active, Duplicated(state, dstate), Const(model), Const(model.time_stepping), Const(model.time_stepping.dt))
+@testset "Soil model: timestep!" begin
+    @time Enzyme.autodiff(set_runtime_activity(Reverse), dostep!, Active, Duplicated(state, dstate), Const(model), Const(model.time_stepping), Const(model.time_stepping.dt))
+    @test all(isfinite.(dstate.temperature))
+end
 
-# TODO: NaNs in the gradient, find out what's wrong 
-@test_broken all(isfinite.(dstate.temperature))
+@testset "Soil energy: FreeWater freezing characteristics" begin
+    # test liquid_water_fraction
+    U = -1e7
+    sat = 1.0
+    por = 0.5
+    Lθ = 3.34e8*sat*por
+    lwc_grad, = Enzyme.autodiff(Reverse, Terrarium.liquid_water_fraction, Active, Const(FreeWater()), Active(U), Const(Lθ))
+    @test lwc_grad[2] ≈ 1 / Lθ
+    ## check case where Lθ is zero
+    Lθ = 0.0
+    lwc_grad, = Enzyme.autodiff(Reverse, Terrarium.liquid_water_fraction, Active, Const(FreeWater()), Active(U), Const(Lθ))
+    @test iszero(lwc_grad[2])
+
+    # test energy_to_temperature
+    ## case 1: frozen
+    sat = 1.0
+    por = 0.5
+    Lθ = 3.34e8*sat*por
+    U = Lθ - 1e7
+    C = 2e5
+    inv_energy_grad, = Enzyme.autodiff(Reverse, Terrarium.energy_to_temperature, Active, Const(FreeWater()), Active(U), Const(Lθ), Const(C))
+    @test inv_energy_grad[2] ≈ 1 / C
+    ## case 2: phase change
+    sat = 1.0
+    por = 0.5
+    Lθ = 3.34e8*sat*por
+    U = -Lθ/2
+    C = 2e5
+    inv_energy_grad, = Enzyme.autodiff(Reverse, Terrarium.energy_to_temperature, Active, Const(FreeWater()), Active(U), Const(Lθ), Const(C))
+    @test iszero(inv_energy_grad[2])
+    ## case 3: thawed
+    sat = 1.0
+    por = 0.5
+    Lθ = 3.34e8*sat*por
+    U = Lθ/2
+    C = 2e5
+    inv_energy_grad, = Enzyme.autodiff(Reverse, Terrarium.energy_to_temperature, Active, Const(FreeWater()), Active(U), Const(Lθ), Const(C))
+    @test inv_energy_grad[2] ≈ 1 / C
+end
