49738bfe8a6c30a0d990a0cd957e1de188cc123c
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-10-01 14:41:16 +0200

    Add methods for converting to RingGrids Fields

--- src/grids/column_ring_grid.jl
+++ src/grids/column_ring_grid.jl
@@ -51,7 +51,7 @@ struct ColumnRingGrid{
         # since most or all land computations will be along this axis
         z_thick = get_spacing(vert)
         z_coords = vcat(-reverse(cumsum(z_thick)), zero(eltype(z_thick)))
-        grid = OceananigansGrids.RectilinearGrid(arch, NF, size=(Nh, Nz), x=(0, 1), z=z_coords, topology=(Periodic, Flat, Bounded))
+        grid = OceananigansGrids.RectilinearGrid(arch, NF, size=(Nh, Nz), x=(1, Nh), z=z_coords, topology=(Periodic, Flat, Bounded))
         # adapt ring grid and mask
         rings = on_architecture(arch, rings)
         mask = on_architecture(arch, mask)
@@ -84,6 +84,33 @@ struct ColumnRingGrid{
 end
 
 get_field_grid(grid::ColumnRingGrid) = grid.grid
+
+"""
+    $SIGNATURES
+
+Converts the given Oceananigans `Field` to a `RingGrids.Field` with a ring grid matching that of 
+"""
+RingGrids.Field(field::Field{LX,LY,Nothing}, grid::ColumnRingGrid; fill_value=NaN) where {LX,LY} = RingGrids.Field(architecture(field), interior(field), grid; fill_value)
+RingGrids.Field(field::AbstractArray, grid::ColumnRingGrid; fill_value=NaN) = RingGrids.Field(architecture(grid), field, grid; fill_value)
+function RingGrids.Field(arch::AbstractArchitecture, field::AbstractArray, grid::ColumnRingGrid; fill_value=NaN)
+    # need to be on CPU to do the copying
+    grid = on_architecture(arch, grid)
+    field = on_architecture(arch, field)
+    # create new RingGrids field initialized with fill_value
+    ring_field = RingGrids.Field(grid.rings)
+    fill!(ring_field, fill_value)
+    # need to access underlying data arrays directly to avoid scalar indexing
+    ring_field.data[grid.mask.data] .= field
+    return ring_field
+end
+
+function on_architecture(arch::AbstractArchitecture, grid::ColumnRingGrid)
+    return ColumnRingGrid(
+        on_architecture(arch, grid.rings),
+        on_architecture(arch, grid.mask),
+        on_architecture(arch, grid.grid)
+    )
+end
 
 function Adapt.adapt_structure(to, grid::ColumnRingGrid)
     return ColumnRingGrid(
