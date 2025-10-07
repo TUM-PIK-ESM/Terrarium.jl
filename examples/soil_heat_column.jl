ed22463a016096339d054b231e76763ae79b43dc
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-10-07 10:59:21 +0200

    Update example scripts

--- examples/soil_heat_column.jl
+++ examples/soil_heat_column.jl
@@ -5,13 +5,14 @@ import CairoMakie as Makie
 grid = ColumnGrid(ExponentialSpacing())
 initializer = FieldInitializers(
     # steady-ish state initial condition for temperature
-    temperature = (x,z) -> -1 - 0.02*z,
+    temperature = (x,z) -> -2 - 0.01*z,
     # fully saturated soil pores
     pore_water_ice_saturation = 1.0,
 )
 # temperature boundary condition
 boundary_conditions = SoilBoundaryConditions(
     grid,
+    # Set a constant temperature of 1°C at the upper boundary
     top = (temperature = ValueBoundaryCondition(1.0),)
 )
 model = SoilModel(; grid, initializer, boundary_conditions)
@@ -21,9 +22,9 @@ sim = initialize(model)
 # run simulation forward for a set period of time
 run!(sim, period=Day(10))
 
-T = interior(sim.state.temperature)
-f = interior(sim.state.liquid_water_fraction)
+T = interior(sim.state.temperature)[1,1,:]
+f = interior(sim.state.liquid_water_fraction)[1,1,:]
 zs = znodes(sim.state.temperature)
 # Plot temperature and liquid fraction profiles
-Makie.scatterlines(T[1,1,:], zs)
-Makie.scatterlines(f[1,1,:], zs)
+Makie.scatterlines(T, zs)
+Makie.scatterlines(f, zs)
