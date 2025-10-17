6d7c5b77b4c36d3ef82455ded29d146d5360b921
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-10-17 17:03:10 +0200

    Remove NF type parameter from AbstractProcess
    Also rename AbstractEnergyBalanceModel -> AbstractSurfaceEnergyBalanceModel

--- src/processes/soil/abstract_types.jl
+++ src/processes/soil/abstract_types.jl
@@ -1,10 +1,10 @@
 # Soil process types
 
 # TODO: Think more about these process types and what methods they should have.
-abstract type AbstractStratigraphy{NF} <: AbstractProcess{NF} end
+abstract type AbstractStratigraphy{NF} <: AbstractProcess end
 
-abstract type AbstractSoilEnergyBalance{NF} <: AbstractProcess{NF} end
+abstract type AbstractSoilEnergyBalance{NF} <: AbstractProcess end
 
-abstract type AbstractSoilHydrology{NF} <: AbstractProcess{NF} end
+abstract type AbstractSoilHydrology{NF} <: AbstractProcess end
 
-abstract type AbstractSoilBiogeochemistry{NF} <: AbstractProcess{NF} end
+abstract type AbstractSoilBiogeochemistry{NF} <: AbstractProcess end
