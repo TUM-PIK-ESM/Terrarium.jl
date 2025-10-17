2563e4977b3af218b98d6af7fbd500cda79da553
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-10-17 13:34:41 +0200

    Rename sim to state in soil_heat_column example

--- examples/soil_heat_column.jl
+++ examples/soil_heat_column.jl
@@ -16,15 +16,15 @@ boundary_conditions = SoilBoundaryConditions(
     top = (temperature = ValueBoundaryCondition(1.0),)
 )
 model = SoilModel(; grid, initializer, boundary_conditions)
-sim = initialize(model)
+state = initialize(model)
 # test one timestep
-@time timestep!(sim)
+@time timestep!(state)
 # run simulation forward for a set period of time
 run!(sim, period=Day(10))
 
-T = interior(sim.state.temperature)[1,1,:]
-f = interior(sim.state.liquid_water_fraction)[1,1,:]
-zs = znodes(sim.state.temperature)
+T = interior(state.state.temperature)[1,1,:]
+f = interior(state.state.liquid_water_fraction)[1,1,:]
+zs = znodes(state.state.temperature)
 # Plot temperature and liquid fraction profiles
 Makie.scatterlines(T, zs)
 Makie.scatterlines(f, zs)
