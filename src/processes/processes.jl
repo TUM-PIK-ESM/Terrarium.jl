e8f5a2c4100377bc38f795e558e4612569384b1d
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-10-18 14:30:36 +0200

    Consolidate abstract types for all processes

--- src/processes/processes.jl
+++ src/processes/processes.jl
@@ -1,3 +1,5 @@
+# Abstract types
+
 include("abstract_types.jl")
 
 export PhysicalConstants
@@ -14,11 +16,36 @@ include("prescribed_atmosphere.jl")
 
 # Soil
 
-include("soil/soil_processes.jl")
+export SoilTexture
+include("soil/biogeochem/soil_texture.jl")
+
+export SoilComposition, volumetric_fractions
+include("soil/biogeochem/soil_composition.jl")
+
+export HomogeneousSoil
+include("soil/biogeochem/homogeneous_soil.jl")
+
+export ConstantSoilCarbonDensity
+include("soil/biogeochem/constant_soil_carbon.jl")
+
+export ConstantHydraulics, SoilHydraulicsSURFEX, UnsatKLinear, UnsatKVanGenuchten
+export saturated_hydraulic_conductivity, mineral_porosity, field_capacity, wilting_point
+include("soil/hydrology/soil_hydraulic_properties.jl")
+
+export SoilHydrology, NoFlow, RichardsEq
+include("soil/hydrology/soil_hydrology.jl")
+include("soil/hydrology/soil_hydrology_rre.jl")
+
+export SoilThermalConductivities, SoilHeatCapacities, SoilThermalProperties, InverseQuadratic
+include("soil/energy/soil_thermal_properties.jl")
+
+export SoilEnergyBalance
+include("soil/energy/soil_energy.jl")
+
+export TemperatureEnergyClosure
+include("soil/energy/temperature_energy_closure.jl")
 
 # Vegetation
-
-include("vegetation/abstract_types.jl")
 
 export PALADYNCarbonDynamics
 include("vegetation/carbon_dynamics.jl")
