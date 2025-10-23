4c12199f01def0680a1612c00f844ae78cead28e
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-10-23 16:22:45 +0200

    Modify formulation of prognostic skinT

--- src/processes/surface_energy/skin_temperature.jl
+++ src/processes/surface_energy/skin_temperature.jl
@@ -1,15 +1,21 @@
 """
     $TYPEDEF
 
-Represents skin temperature prescribed from input data.
+Represents skin temperature prescribed by an input variable.
 """
 struct PrescribedSkinTemperature <: AbstractSkinTemperature end
 
 variables(::PrescribedSkinTemperature) = (
     auxiliary(:ground_heat_flux, XY(), units=u"W/m^2", desc="Ground heat flux"),
     input(:skin_temperature, XY(), units=u"°C", desc="Longwave emission temperature of the land surface in °C"),
-    input(:ground_temperature, XY(), units=u"°C", desc="Temperature of the uppermost ground or soil grid cell in °C")
 )
+
+function compute_auxiiliary!(state, model, skinT::AbstractSkinTemperature)
+    grid = get_grid(model)
+    rad = get_radiative_fluxes(model)
+    tur = get_turbulent_fluxes(model)
+    launch!(grid, compute_ground_heat_flux!, state, grid, skinT, rad, tur)
+end
 
 """
     $TYPEDEF
@@ -33,18 +39,6 @@ variables(::PrognosticSkinTemperature) = (
     input(:ground_temperature, XY(), units=u"°C", desc="Temperature of the uppermost ground or soil grid cell in °C")
 )
 
-function compute_auxiiliary!(state, model, skinT::PrognosticSkinTemperature)
-    grid = get_grid(model)
-    rad = get_radiative_fluxes(model)
-    tur = get_turbulent_fluxes(model)
-    launch!(grid, compute_ground_heat_flux!, state, grid, skinT, rad, tur)
-end
-
-function compute_tendencies!(state, model, ::PrognosticSkinTemperature)
-    grid = get_grid(model)
-    launch!(grid, compute_skin_temperature_tendency!, state, grid, skinT)
-end
-
 # Kernels
 
 @kernel function compute_ground_heat_flux!(
@@ -61,33 +55,30 @@ end
     state.ground_heat_flux[i, j] = -R_net - H_s - H_l
 end
 
-@kernel function compute_skin_temperature_tendency!(
-    state, grid,
-    skinT::PrognosticSkinTemperature
-)
-    i, j = idx = @index(Global, NTuple)
-    # compute and accumulate tendency
-    state.tendencies.skin_temperature[i, j] += skin_temperature_tendency(idx, state, grid, skinT)
-end
-
 # Kernel functions
-
-@inline function skin_temperature_tendency(idx, state, grid, skinT::PrognosticSkinTemperature)
-    i, j = idx
-    # get thickness of topmost soil/ground grid cell
-    field_grid = get_field_grid(grid)
-    Δz₁ = Δzᵃᵃᶜ(i, j, field_grid.Nz, field_grid)
-    # get ground heat flux
-    G = state.ground_heat_flux[i, j]
-    # compute correction ΔTₛ by setting G equal to the half-cell heat flux;
-    # note that this is technically not a tendency and should *not* be multiplied by Δt!
-    κₛ = skin_thermal_conductivity(idx, state, skinT)
-    ΔTₛ = -G * Δz₁ / (2*κₛ)
-    return ΔTₛ
-end
 
 @inline skin_thermal_conductivity(idx, state, skinT::PrognosticSkinTemperature) = skinT.κₛ
 
 @inline @inbounds skin_temperature(idx, state, ::AbstractSkinTemperature) = state.skin_temperature[idx]
 
 @inline @inbounds ground_heat_flux(idx, state, ::AbstractSkinTemperature) = state.ground_heat_flux[idx]
+
+"""
+    implicit_skin_temperature(idx, state, grid, skinT::PrognosticSkinTemperature)
+
+Diagnose the skin temperature implied by the current `ground_heat_flux` and `ground_temperature`.
+"""
+@inline function implicit_skin_temperature(idx, state, grid, skinT::PrognosticSkinTemperature)
+    i, j = idx
+    # get thickness of topmost soil/ground grid cell
+    field_grid = get_field_grid(grid)
+    Δz₁ = Δzᵃᵃᶜ(i, j, field_grid.Nz, field_grid)
+    # get ground heat flux
+    G = state.ground_heat_flux[i, j]
+    # get ground temperature
+    Tₛ = state.ground_temperature[i, j]
+    # compute new skin temperature T₀ by setting G equal to the half-cell heat flux
+    κₛ = skin_thermal_conductivity(idx, state, skinT)
+    T₀ = Tₛ - G * Δz₁ / (2*κₛ)
+    return T₀
+end
