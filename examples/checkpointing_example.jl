b62bdb99a74c63b79f66e058ab1d37df475bf7da
Max <maximilian.gelbrecht@posteo.de>
2025-11-25 11:46:49 +0100

    checkpointing ext and example

---
+++ examples/checkpointing_example.jl
@@ -0,0 +1,26 @@
+using Terrarium, Checkpointing, Enzyme
+
+import CairoMakie as Makie
+
+grid = ColumnGrid(ExponentialSpacing())
+initializer = FieldInitializers(
+    # steady-ish state initial condition for temperature
+    temperature = (x,z) -> -1 - 0.01*z,
+    # fully saturated soil pores
+    saturation_water_ice = 1.0,
+)
+model = SoilModel(grid; initializer)
+# constant surface temperature of 1°C
+bcs = PrescribedSurfaceTemperature(:T_ub, 1.0)
+integrator = initialize(model, ForwardEuler(), boundary_conditions=bcs)
+scheme = Revolve(1)
+N_t = 100
+
+dintegrator = make_zero(integrator)
+# set a one hot seed for a sensitivity analysis of T for now 
+interior(dintegrator.state.temperature)[1,1,2] = 1.0
+
+autodiff(set_runtime_activity(Reverse), run!, Const, Duplicated(integrator, dintegrator), Const(scheme), Const(N_t))
+
+dU = interior(dintegrator.state.internal_energy)[1,1,:]
+dT = interior(dintegrator.state.temperature)[1,1,:]
---
+++ ext/TerrariumCheckpointingExt/TerrariumCheckpointingExt.jl
@@ -0,0 +1,33 @@
+module TerrariumCheckpointingExt
+
+using DocStringExtensions
+using Checkpointing
+
+using Terrarium
+using Terrarium: run!
+
+"""
+    $SIGNATURES
+
+Run the simulation for `steps` or a given time `period` with timestep size `Δt` (in seconds or Dates.Period).
+
+Uses a `checkpointing_scheme` from Checkpointing.jl to checkpoint the integrator state for automatic differentiation with Enzyme.jl. 
+"""
+function Terrarium.run!(
+        integrator::ModelIntegrator,
+        checkpointing_scheme::Checkpointing.Scheme,
+        steps::Int,
+        Δt = default_dt(timestepper(integrator))
+    )
+    Δt = Terrarium.convert_dt(Δt)
+
+    @ad_checkpoint checkpointing_scheme for _ in 1:steps
+        timestep!(integrator, Δt, finalize = false)
+    end
+
+    # Update auxiliary variables for final timestep
+    compute_auxiliary!(integrator.state, integrator.model)
+    return integrator
+end
+
+end 
