088511ee1087f7c0757d7369712100514ee5ff6e
mahabadri <maha_badri@yahoo.fr>
2025-12-16 19:36:26 +0100

    add evaporation

--- src/models/surface/surface_hydrology_model.jl
+++ src/models/surface/surface_hydrology_model.jl
@@ -8,10 +8,11 @@ $TYPEDFIELDS
 """
 @kwdef struct SurfaceHydrologyModel{
     NF,
+    GridType<:AbstractLandGrid{NF},
+    Atmosphere<:AbstractAtmosphere,
+    SurfaceEnergyBalance<:AbstractSurfaceEnergyBalance,
     CanopyHydrology<:AbstractCanopyHydrology,
     SurfaceRunoff<:AbstractSurfaceRunoff,
-    Atmosphere<:AbstractAtmosphere,
-    GridType<:AbstractLandGrid{NF},
     Constants<:PhysicalConstants{NF},
     Initializer<:AbstractInitializer,
 } <: AbstractSurfaceHydrologyModel{NF, GridType}
@@ -21,11 +22,14 @@ $TYPEDFIELDS
     "Atmospheric input configuration"
     atmosphere::Atmosphere = PrescribedAtmosphere(eltype(grid))
 
+    "Surface energy balance scheme"
+    surface_energy_balance::SurfaceEnergyBalance = SurfaceEnergyBalance(eltype(grid))
+
     "Canopy hydrology scheme"
     canopy_hydrology::CanopyHydrology = PALADYNCanopyHydrology(eltype(grid))
     
     "Surface runoff scheme"
-    surface_runoff::SurfaceRunoff = SurfaceRunoff(eltype(grid))
+    surface_runoff::SurfaceRunoff = PALADYNSurfaceRunoff(eltype(grid))
 
     "Physical constants"
     constants::Constants = PhysicalConstants(eltype(grid))
@@ -34,8 +38,12 @@ $TYPEDFIELDS
     initializer::Initializer = DefaultInitializer()
 end
 
-# SurfaceHydrologyModel getter methods
+# Getter methods
+get_grid(model::SurfaceHydrologyModel) = model.grid
+
 get_atmosphere(model::SurfaceHydrologyModel) = model.atmosphere
+
+get_surface_energy_balance(model::SurfaceHydrologyModel) = model.surface_energy_balance
 
 get_canopy_hydrology(model::SurfaceHydrologyModel) = model.canopy_hydrology
 
@@ -45,25 +53,29 @@ get_constants(model::SurfaceHydrologyModel) = model.constants
 
 # Model interface methods
 variables(model::SurfaceHydrologyModel) = tuplejoin(
-    variables(model.atmosphere)...,
-    variables(model.canopy_hydrology)...,
-    variables(model.surface_runoff)...,
+    variables(model.atmosphere),
+    variables(model.surface_energy_balance),
+    variables(model.canopy_hydrology),
+    variables(model.surface_runoff),
 )
 
 get_processes(model::SurfaceHydrologyModel) = (
     model.atmosphere,
+    model.surface_energy_balance,
     model.canopy_hydrology,
     model.surface_runoff
 )
 
 function compute_auxiliary!(state, model::SurfaceHydrologyModel)
     compute_auxiliary!(state, model, model.atmosphere)
+    compute_auxiliary!(state, model, model.surface_energy_balance)
     compute_auxiliary!(state, model, model.canopy_hydrology)
     compute_auxiliary!(state, model, model.surface_runoff)
 end
 
 function compute_tendencies!(state, model::SurfaceHydrologyModel)
     compute_tendencies!(state, model, model.atmosphere)
+    compute_tendencies!(state, model, model.surface_energy_balance)
     compute_tendencies!(state, model, model.canopy_hydrology)
     compute_tendencies!(state, model, model.surface_runoff)
 end
--- src/processes/surface_hydrology/canopy_hydrology.jl
+++ src/processes/surface_hydrology/canopy_hydrology.jl
@@ -1,8 +1,7 @@
 """
     $TYPEDEF
 
-Canopy hydrology implementation following PALADYN (Willeit 2016) considering only canopy interception and ignoring 
-canopy evaporation for now.
+Canopy hydrology implementation following PALADYN (Willeit 2016) considering only liquid water (no snow).
 
 Properties:
 $TYPEDFIELDS
@@ -15,6 +14,9 @@ $TYPEDFIELDS
     "Extinction coefficient for radiation"
     k_ext::NF = 0.5
 
+    "Canopy interception capacity parameter, Verseghy 1991 [kg/m²]"
+    can_max_w::NF = 0.2
+
     "Canopy water removal timescale [s]"
     τ_w::NF = 86400.0
 end
@@ -24,6 +26,7 @@ PALADYNCanopyHydrology(::Type{NF}; kwargs...) where {NF} = PALADYNCanopyHydrolog
 variables(::PALADYNCanopyHydrology) = (
     prognostic(:w_can, XY(); desc="Canopy liquid water", units=u"kg/m^2"), 
     auxiliary(:I_can, XY(); desc="Canopy rain interception", units=u"kg/m^2/s"), 
+    auxiliary(:E_can, XY(); desc="Canopy evaporation", units=u"kg/m^2/s"),
     input(:LAI, XY(); desc="Leaf Area Index", units=u"m^2/m^2"), 
     input(:SAI, XY(); desc="Stem Area Index", units=u"m^2/m^2")
 )
@@ -34,46 +37,74 @@ variables(::PALADYNCanopyHydrology) = (
 
 Computes `I_can`, the canopy rain interception, following Eq. 42, PALADYN (Willeit 2016).
 """
-@inline function compute_I_can(canopy_hydrology::PALADYNCanopyHydrology{NF}, rainfall, LAI, SAI) where NF   
-    I_can = canopy_hydrology.α_int * rainfall * (1 - exp(-canopy_hydrology.k_ext*(LAI + SAI))) 
+@inline function compute_I_can(canopy_hydrology::PALADYNCanopyHydrology{NF}, precip, LAI, SAI) where NF   
+    I_can = canopy_hydrology.α_int * precip * (one(NF) - exp(-canopy_hydrology.k_ext*(LAI + SAI))) 
     return I_can
 end
 
 """
     $SIGNATURES
-Computes the `w_can` tendency following Eq. 41, PALADYN (Willeit 2016).
-Modified to ignore the evaporation term for now.
-"""
-@inline function compute_w_can_tend(canopy_hydrology::PALADYNCanopyHydrology{NF}, rainfall, LAI, SAI, w_can) where NF
-    # Compute I_can
-    I_can = compute_I_can(canopy_hydrology, rainfall, LAI, SAI)
 
-    # Compute w_can tendency
-    w_can_tendency = I_can - w_can / canopy_hydrology.τ_w
+Computes `E_can`, the canopy evaporation, following Eq. 43, PALADYN (Willeit 2016).
+"""
+@inline function compute_E_can(
+    canopy_hydrology::PALADYNCanopyHydrology{NF}, 
+    constants::PhysicalConstants{NF},   
+    w_can, LAI, SAI, rₐ, q_sat, q_air
+    ) where NF 
+    # Compute the wet canopy fraction
+    w_can_max = canopy_hydrology.can_max_w * (LAI + SAI) 
+    f_can = w_can / w_can_max
+    
+    # Compute the canopy evaporation
+    E_can = (constants.ρₐ / rₐ) * (q_sat - q_air) * f_can
+    return E_can
+end
+
+
+"""
+    $SIGNATURES
+Computes the `w_can` tendency following Eq. 41, PALADYN (Willeit 2016).
+"""
+@inline function compute_w_can_tend(canopy_hydrology::PALADYNCanopyHydrology{NF}, w_can, I_can, E_can) where NF
+    
+    w_can_tendency = I_can - E_can - w_can / canopy_hydrology.τ_w
 
     return w_can_tendency
 end
 
 function compute_auxiliary!(state, model, canopy_hydrology::PALADYNCanopyHydrology)
     grid = get_grid(model)
-    launch!(grid, :xy, compute_auxiliary_kernel!, state, canopy_hydrology)
+    atmos = get_atmosphere(model)
+    seb = get_surface_energy_balance(model)
+    constants = get_constants(model)
+    launch!(grid, :xy, compute_auxiliary_kernel!, state, canopy_hydrology, constants, atmos, seb)
 end
 
 @kernel function compute_auxiliary_kernel!(
     state, 
     canopy_hydrology::PALADYNCanopyHydrology{NF},
+    constants::PhysicalConstants{NF},
     atmos::AbstractAtmosphere,
-    ) where NF
+    seb::AbstractSurfaceEnergyBalance           
+) where NF
     
     i, j = @index(Global, NTuple)
 
     # Get inputs 
-    rainfall = rainfall(i, j, state, atmos)
+    precip = rainfall(i, j, state, atmos)
+    q_air = specific_humidity(i, j, state, atmos)
+    rₐ = aerodynamic_resistance(i, j, state, seb.turbulent_fluxes.aerodynamic_resistance)
+    q_sat = specific_humidity_saturation(i, j, state, seb.skin_temperature, atmos, constants)
     LAI = state.LAI[i, j]
     SAI = state.SAI[i, j]
+    w_can = state.w_can[i, j]
 
     # Compute canopy rain interception
-    state.I_can[i, j] = compute_I_can(canopy_hydrology, rainfall, LAI, SAI)
+    state.I_can[i, j] = compute_I_can(canopy_hydrology, precip, LAI, SAI)
+    
+    # Compute canopy evaporation for diagnostic output
+    state.E_can[i, j] = compute_E_can(canopy_hydrology, constants, w_can, LAI, SAI, rₐ, q_sat, q_air)
 end
 
 function compute_tendencies!(state, model, canopy_hydrology::PALADYNCanopyHydrology)
@@ -83,17 +114,15 @@ end
 
 @kernel function compute_tendencies_kernel!(
     state, 
-    canopy_hydrology::PALADYNCanopyHydrology{NF},
-    atmos::AbstractAtmosphere
-    ) where NF  
+    canopy_hydrology::PALADYNCanopyHydrology{NF}
+) where NF  
     i, j = @index(Global, NTuple)
 
     # Get inputs
-    rainfall = rainfall(i, j, state, atmos)
-    LAI = state.LAI[i, j]
-    SAI = state.SAI[i, j]
     w_can = state.w_can[i, j]
+    I_can = state.I_can[i, j]
+    E_can = state.E_can[i, j]
 
     # Compute canopy water tendency
-    state.tendencies.w_can[i, j] = compute_w_can_tend(canopy_hydrology, rainfall, LAI, SAI, w_can)
+    state.tendencies.w_can[i, j] = compute_w_can_tend(canopy_hydrology, w_can, I_can, E_can)
 end
