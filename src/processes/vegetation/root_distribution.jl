ec5cd83d41de527ac11c5fc8ada9bfbdfe468b84
Brian Groenke <brian.groenke@pik-potsdam.de>
2026-01-20 19:30:18 +0100

    Rename parameters in root distribution

--- src/processes/vegetation/root_distribution.jl
+++ src/processes/vegetation/root_distribution.jl
@@ -6,17 +6,21 @@ based on the scheme proposed by Zeng (2001). The PDF of the root distribution
 is modeled as
 
 ```math
-\\frac{\\partial R}{\\partial z} = \\frac{1}{2} \\left( d_1 \\exp(d_1 z) + d_2 \\exp(d_2 z) \\right)
+\\frac{\\partial R}{\\partial z} = \\frac{1}{2} \\left( a \\exp(a z) + b \\exp(b z) \\right)
 ```
 which is then integrated over the soil column and normalized to sum to unity. Note that
-this is effectively the average of two exponential distributions with rates `d1` and `d2`.
+this is effectively the average of two exponential distributions with rates `a` and `b`, both
+with units m⁻¹. The resulting CDF of this distribution determines the root distribution.
 
 Properties:
-$TYPEDFIELDS
+$FIELDS
 """
 @kwdef struct StaticExponentialRootDistribution{NF} <: AbstractRootDistribution
-    d1::NF = 7.0 # TODO PFT-specific parameter (here needleleaf tree)
-    d2::NF = 2.0 # TODO PFT-specific parameter (here needleleaf tree)
+    "First empirical rate parameter for root distribution"
+    a::NF = 7.0 # TODO PFT-specific parameter (here needleleaf tree)
+    
+    "Second empirical rate parameter for root distribution"
+    b::NF = 2.0 # TODO PFT-specific parameter (here needleleaf tree)
 end
 
 StaticExponentialRootDistribution(::Type{NF}; kwargs...) where {NF} = StaticExponentialRootDistribution{NF}(; kwargs...)
@@ -32,7 +36,7 @@ function root_fraction(roots::StaticExponentialRootDistribution{NF}, grid::Abstr
     fgrid = get_field_grid(grid)
     # define pdf of root distribution as a continuous function of depth
     ∂R∂z = FunctionField(fgrid, parameters=roots) do x, z, p
-        0.5 * (p.d1 * exp(p.d1 * z) + p.d2 * exp(p.d2 * z))
+        0.5 * (p.d1 * exp(p.a * z) + p.d2 * exp(p.b * z))
     end
     # scale by the layer thicknesses
     Δz = zspacings(fgrid, Center(), Center(), Center())
