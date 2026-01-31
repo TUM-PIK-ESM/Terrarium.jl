3e39f44554a5f52467b3ef2d4c550a2f325d601e
Brian Groenke <brian.groenke@pik-potsdam.de>
2026-01-31 16:49:17 +0100

    Add tests for some state variable utilities

--- test/state_variables.jl
+++ test/state_variables.jl
@@ -1,47 +1,53 @@
-module StateVariablesTests
-
 using Terrarium
 using Test
 
-import Terrarium: AbstractLandGrid, VarDims, XY, XYZ, prognostic, auxiliary, input, namespace
+using Terrarium: AbstractLandGrid, VarDims, XY, XYZ, prognostic, auxiliary, input, namespace
 
 DEFAULT_NF = Float32
 
-@kwdef struct SubModel{NF, Grid<:AbstractLandGrid{NF}} <: Terrarium.AbstractModel{NF, Grid}
-    grid::Grid
-    initializer = DefaultInitializer()
+module StateVariablesTestTypes
+
+    using Terrarium
+    using Terrarium: AbstractLandGrid, VarDims, XY, XYZ, prognostic, auxiliary, input, namespace
+
+    using Test
+
+    @kwdef struct SubModel{NF, Grid<:AbstractLandGrid{NF}} <: Terrarium.AbstractModel{NF, Grid}
+        grid::Grid
+        initializer = DefaultInitializer()
+    end
+
+    Terrarium.variables(model::SubModel) = (
+        # duplicate naming allowed in new namesapce
+        auxiliary(:auxvar2D, XY()),
+        # inputs are handled "globally" (i.e. all inputs with a given name refer to the same field)
+        input(:forcing, XY()),
+    )
+
+    @kwdef struct TestModel{NF, Grid<:AbstractLandGrid{NF}} <: Terrarium.AbstractModel{NF, Grid}
+        grid::Grid
+        submodel = SubModel(; grid)
+        initializer = DefaultInitializer()
+    end
+
+    struct TestClosure <: Terrarium.AbstractClosureRelation end
+
+    Terrarium.closurevar(::TestClosure) = auxiliary(:closurevar, XYZ())
+
+    Terrarium.variables(model::TestModel) = (
+        prognostic(:progvar3D, XYZ()),
+        prognostic(:cprogvar3D, XYZ(), closure=TestClosure()),
+        prognostic(:progvar2D, XY()),
+        auxiliary(:auxvar3D, XYZ()),
+        auxiliary(:auxvar2D, XY()),
+        input(:forcing, XY()),
+        namespace(:submodel, variables(model.submodel)),
+    )
 end
-
-Terrarium.variables(model::SubModel) = (
-    # duplicate naming allowed in new namesapce
-    auxiliary(:auxvar2D, XY()),
-    # inputs are handled "globally" (i.e. all inputs with a given name refer to the same field)
-    input(:forcing, XY()),
-)
-
-@kwdef struct TestModel{NF, Grid<:AbstractLandGrid{NF}} <: Terrarium.AbstractModel{NF, Grid}
-    grid::Grid
-    submodel = SubModel(; grid)
-    initializer = DefaultInitializer()
-end
-
-struct TestClosure <: Terrarium.AbstractClosureRelation end
-
-Terrarium.closurevar(::TestClosure) = auxiliary(:closurevar, XYZ())
-
-Terrarium.variables(model::TestModel) = (
-    prognostic(:progvar3D, XYZ()),
-    prognostic(:cprogvar3D, XYZ(), closure=TestClosure()),
-    prognostic(:progvar2D, XY()),
-    auxiliary(:auxvar3D, XYZ()),
-    auxiliary(:auxvar2D, XY()),
-    input(:forcing, XY()),
-    namespace(:submodel, variables(model.submodel)),
-)
 
 @testset "State variable initialization" begin
     grid = ColumnGrid(CPU(), DEFAULT_NF, ExponentialSpacing(N=10))
-    model = TestModel(; grid)
+    model = StateVariablesTestTypes.TestModel(; grid)
     state = initialize(model)
     # Check that all prognostic variables are defined correctly
     @test hasproperty(state, :progvar3D) && isa(state.prognostic.progvar3D, Field{Center,Center,Center})
@@ -63,4 +69,45 @@ Terrarium.variables(model::TestModel) = (
     @test hasproperty(state.submodel, :auxvar2D) && isa(state.submodel.auxvar2D, Field{Center,Center,Nothing})
 end
 
+@testset "State variable utilities" begin
+    grid = ColumnGrid(CPU(), DEFAULT_NF, ExponentialSpacing(N=10))
+    model = StateVariablesTestTypes.TestModel(; grid)
+    state = initialize(model)
+    for input in state.inputs
+        set!(input, 1)
+    end
+
+    # test fill!
+    fill!(state, 2)
+    @test all(map(field -> all(field .== 2), state.prognostic))
+    @test all(map(field -> all(field .== 2), state.auxiliary))
+    @test all(map(field -> all(field .== 2), state.tendencies))
+    @test all(map(field -> all(field .== 1), state.inputs)) # inputs should not be modified
+
+    # test copyto!
+    state2 = initialize(model)
+    copyto!(state2, state)
+    @test all(map(field -> all(field .== 2), state2.prognostic))
+    @test all(map(field -> all(field .== 2), state2.auxiliary))
+    @test all(map(field -> all(field .== 2), state2.tendencies))
+    @test all(map(field -> all(field .== 1), state2.inputs))
+
+    # test field getters
+    fields = get_fields(state, :progvar3D, :submodel => (:auxvar2D,))
+    @test hasproperty(fields, :progvar3D)
+    @test hasproperty(fields, :submodel)
+    @test hasproperty(fields.submodel, :auxvar2D)
+    prog_fields = Terrarium.prognostic_fields(state, model)
+    @test length(prog_fields) == 3 # note that namespaces are NOT included
+    @test hasproperty(prog_fields, :progvar3D)
+    @test hasproperty(prog_fields, :cprogvar3D)
+    @test hasproperty(prog_fields, :progvar2D)
+    aux_fields = Terrarium.auxiliary_fields(state, model)
+    @test length(aux_fields) == 2 # note that namespaces are NOT included
+    @test hasproperty(aux_fields, :auxvar3D)
+    @test hasproperty(aux_fields, :auxvar2D)
+    input_fields = Terrarium.input_fields(state, model)
+    @test length(input_fields) == 1 # note that namespaces are NOT included
+    @test hasproperty(input_fields, :forcing) 
 end
+
