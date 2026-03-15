dd2327a1ce9f5271c916c6ef27d0165b39b71a95
Brian Groenke <brian.groenke@pik-potsdam.de>
2026-03-15 12:53:12 +0100

    Add more docs for soil types/method interfaces

--- src/processes/soil/abstract_types.jl
+++ src/processes/soil/abstract_types.jl
@@ -44,6 +44,13 @@ Return the biogeochemistry scheme associated with `soil`.
 
 # Soil process types
 
+"""
+    $TYPEDEF
+
+Base type for soil energy balance process implementations. Subtypes should define
+state variables for soil `temperature`, `internal_energy`, and any other relevant
+thermal properties or state variables.
+"""
 abstract type AbstractSoilEnergyBalance{NF} <: AbstractProcess{NF} end
 
 """
@@ -60,9 +67,80 @@ Compute the thermal conductivity at index `i, j, k`.
 """
 function compute_thermal_conductivity end
 
+"""
+    $TYPEDEF
+
+Base type for soil hydrology implementations. Subtypes should define
+state variables for `saturation_water_ice`, `hydraulic_conductivity`, 
+`liquid_water_fraction`, and the current `water_table` level, along with
+any other implementation-specific state variables.
+"""
 abstract type AbstractSoilHydrology{NF} <: AbstractProcess{NF} end
 
+"""
+    get_swrc(hydrology::AbstractSoilHydrology)
+
+Return the soil water retention curve from the `hydraulic_properties` associated with
+the given soil hydrology configuration.
+"""
+function get_swrc end
+
+"""
+    get_hydraulic_properties(hydrology::AbstractSoilHydrology)
+
+Return the soil hydraulic properties defined by the given soil `hydrology` configuration.
+"""
+function get_hydraulic_properties end
+
+"""
+    saturation_water_ice(i, j, k, grid, fields, ::AbstractSoilHydrology)
+
+Compute or retrieve the current saturation level of water + ice in the pore space.
+"""
+function saturation_water_ice end
+
+"""
+    hydraulic_conductivity(i, j, k, grid, fields, ::AbstractSoilHydrology)
+
+Compute or retrieve the current hydraulic conductivity at grid cell `i, j` and vertical
+layer **face** `k`.
+"""
+function hydraulic_conductivity end
+
+"""
+    liquid_water_fraction(i, j, k, grid, fields, ::AbstractSoilHydrology)
+
+Compute or retrieve the current fraction of unfrozen water in the pore space.
+"""
+function liquid_water_fraction end
+
+"""
+    water_table(i, j, k, grid, fields, ::AbstractSoilHydrology)
+
+Compute or retrieve the current water table level relative to the surface.
+"""
+function water_table end
+
+"""
+    surface_excess_water(i, j, k, grid, fields, ::AbstractSoilHydrology)
+
+Retrieve the current saturation level of water + ice in the pore space.
+"""
+function surface_excess_water end
+
+"""
+    $TYPEDEF
+
+Base type for soil biogeochemistry process implementations.
+"""
 abstract type AbstractSoilBiogeochemistry{NF} <: AbstractProcess{NF} end
+
+"""
+    density_soc(i, j, k, grid, fields, bgc::AbstractSoilBiogeochemistry)
+
+Compute or return the soil organic carbon density at grid cell index `i, j, k`.
+"""
+function density_soc end
 
 # Parameterization types
 
@@ -120,8 +198,8 @@ function soil_matrix end
 """
     soil_volume(i, j, k, grid, fields, ::AbstractStratigraphy, args...)
 
-Return a description of the full material composition of the soil volume at index `i, j, k` for the
-given stratigraphy parameterization.
+Return a [`SoilVolume`](@ref) describing the full material composition of the soil volume at index
+`i, j, k` for the given stratigraphy parameterization.
 """
 function soil_volume end
 
