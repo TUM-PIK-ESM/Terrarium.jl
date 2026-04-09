29c2b8cbeeb6fdbb1fb5526fbe16150d53772390
Olivier Bonte <olivier.bonte@hotmail.com>
2026-04-09 10:18:11 +0200

    Revert "Add variable constructions functions export"
    This reverts commit 38b19064da03ea88359d53bf01e1594f116bf61b.

--- src/Terrarium.jl
+++ src/Terrarium.jl
@@ -86,7 +86,7 @@ include("utils/utils.jl")
 # debugging utilities
 include("diagnostics/debugging.jl")
 
-export PrognosticVariable, AuxiliaryVariable, InputVariable, Input, XY, XYZ, prognostic, auxiliary, input
+export PrognosticVariable, AuxiliaryVariable, InputVariable, Input, XY, XYZ
 include("abstract_variables.jl")
 
 # grids
