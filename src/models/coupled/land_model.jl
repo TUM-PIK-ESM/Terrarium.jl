1f985e7d8649af670a0bfadf2f6d4b437a7c5ceb
Max <maximilian.gelbrecht@posteo.de>
2026-06-02 17:29:37 +0200

    update constructor in landmodel

--- src/models/coupled/land_model.jl
+++ src/models/coupled/land_model.jl
@@ -50,6 +50,7 @@ end
 function StateVariables(
         model::LandModel{NF};
         clock = Clock(time = zero(NF)),
+        timestepper_classes = (;),
         boundary_conditions = (;),
         fields = (;),
         input_variables = ()
@@ -66,7 +67,7 @@ function StateVariables(
     bcs = merge_boundary_conditions(boundary_conditions, ground_heat_flux_bc, infiltration_bc)
     # Merge user-defined fields with BC fields
     fields = merge((; ground_heat_flux, infiltration), fields)
-    return StateVariables(vars, grid; clock, timesteppers = get_timesteppers(model), boundary_conditions = bcs, fields)
+    return StateVariables(vars, grid; clock, timesteppers = get_timesteppers(model), timestepper_classes, boundary_conditions = bcs, fields)
 end
 
 function initialize!(state, model::LandModel)
