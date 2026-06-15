09943a1e76825ca6092e553342120fb63de8397b
Olivier Bonte <olivier.bonte@hotmail.com>
2026-06-15 16:39:37 +0200

    Fix mistake tests

--- test/thermodynamics_test.jl
+++ test/thermodynamics_test.jl
@@ -13,11 +13,11 @@ using Test
     # Clima
     params_clima = Thermodynamics.Parameters.ThermodynamicsParameters(FT)
     e_sat_clima = Thermodynamics.saturation_vapor_pressure(params_clima, air_temperature_K)
-    q_sat_clima = Thermodynamics.q_vap_saturation(params_clima, air_temperature_K, air_pressure)
+    q_sat_clima = Thermodynamics.q_vap_saturation(params_clima, air_temperature_K, air_density)
     # Terrarium
     thermodyn_constants = Terrarium.ThermodynamicConstants(FT)
     e_sat_terrarium = Terrarium.saturation_vapor_pressure(thermodyn_constants, air_temperature)
-    q_sat_terrarium = Terrarium.saturation_specific_humidity_vapor(thermodyn_constants, air_temperature_K, air_pressure)
+    q_sat_terrarium = Terrarium.saturation_specific_humidity_vapor(thermodyn_constants, air_temperature, air_density)
     # Compare
     @test e_sat_clima ≈ e_sat_terrarium
     @test q_sat_clima ≈ q_sat_terrarium
@@ -32,9 +32,9 @@ end
     e_sat_tetens = 610.78 * exp(17.27 * air_temperature / (air_temperature + 237.3)) # Pa
     q_sat_tetens = 0.62 * e_sat_tetens / (air_pressure - 0.38 * e_sat_tetens) # kg/kg
     # Using Terrarium function
-    e_sat_clima = Terrarium.saturation_vapor_pressure(thermodyn_constants, air_temperature)
-    q_sat_clima = Terrarium.saturation_specific_humidity_vapor(thermodyn_constants, air_temperature, air_pressure)
+    e_sat = Terrarium.saturation_vapor_pressure(thermodyn_constants, air_temperature)
+    q_sat = Terrarium.saturation_specific_humidity_vapor(thermodyn_constants, air_temperature, air_density)
     # Compare with 10% tolerance
-    @test isapprox(e_sat_clima, e_sat_tetens; rtol = 0.1)
-    @test isapprox(q_sat_clima, q_sat_tetens; rtol = 0.1)
+    @test isapprox(e_sat, e_sat_tetens; rtol = 0.1)
+    @test isapprox(q_sat, q_sat_tetens; rtol = 0.1)
 end
