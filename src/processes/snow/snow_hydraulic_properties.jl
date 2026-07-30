c7a78f2acf0f5b2537f1b8b569e80b757bd8f5e5
Olivier Bonte <olivier.bonte@hotmail.com>
2026-07-30 16:57:02 +0200

    Change snow hydraulic properties to defaults from Tarboton

--- src/processes/snow/snow_hydraulic_properties.jl
+++ src/processes/snow/snow_hydraulic_properties.jl
@@ -10,14 +10,18 @@ abstract type AbstractSnowHydraulics{NF} end
 
 Constant, spatially homogeneous snow hydraulic properties: a saturated hydraulic conductivity `K_sat`
 and a capillary retention `L_c`, setting the Darcy-type meltwater outflow (see
-[`compute_meltwater_outflow`](@ref)).
+[`compute_meltwater_outflow`](@ref)). Default values follow [tarbotonSpatiallyDistributedEnergy1994](@cite).
 
 Properties:
 $TYPEDFIELDS
+
+# References
+
+* [tarbotonSpatiallyDistributedEnergy1994](@cite) Tarboton, Chowdhury and Jackson (1994)
 """
 @parameterized @kwdef struct ConstantSnowHydraulics{NF} <: AbstractSnowHydraulics{NF}
     "Hydraulic conductivity at saturation"
-    @param saturated_conductivity::NF = 1.0e-5 (units = u"m/s", bounds = Positive, scale = 1.0e-5)
+    @param saturated_conductivity::NF = 20.0 / 3600.0 (units = u"m/s", bounds = Positive, scale = 1.0e-3)
 
     "Capillary retention `liq_c`: liquid fraction held against gravity before meltwater drains"
     @param capillary_retention::NF = 0.05 (bounds = UnitInterval,)
