6a79e0d43a6e8eaeac8e32b6e37cca630ce57885
Brian Groenke <brian.groenke@pik-potsdam.de>
2026-08-15 01:52:03 +0200

    Fix another convert_dt call

--- ext/TerrariumCheckpointingExt/TerrariumCheckpointingExt.jl
+++ ext/TerrariumCheckpointingExt/TerrariumCheckpointingExt.jl
@@ -14,12 +14,12 @@ Run the simulation for `steps` with timestep size `Δt` (in seconds or Dates.Per
 Uses a `checkpointing_scheme` from Checkpointing.jl to checkpoint the integrator state for automatic differentiation with Enzyme.jl. 
 """
 function Terrarium.run!(
-        integrator::ModelIntegrator,
+        integrator::ModelIntegrator{NF},
         checkpointing_scheme::Checkpointing.Scheme,
         steps::Int,
         Δt = default_dt(integrator)
-    )
-    Δt = Terrarium.convert_dt(Δt)
+    ) where {NF}
+    Δt = Terrarium.convert_dt(NF, Δt)
 
     @ad_checkpoint checkpointing_scheme for _ in 1:steps
         timestep!(integrator, Δt; finalize = false)
