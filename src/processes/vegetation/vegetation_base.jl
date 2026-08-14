c589bce2f5bd41fe67022278c5281b9267757c21
Brian Groenke <brian.groenke@pik-potsdam.de>
2026-08-14 18:22:08 +0200

    Fix buggy dispatch for vegetation_area_fraction

--- src/processes/vegetation/vegetation_base.jl
+++ src/processes/vegetation/vegetation_base.jl
@@ -2,17 +2,7 @@
 
 # TODO: Remove once dedicated vegetation surface parameterizations are added
 # Also may be a good reason to rename VegetationCarbonCycle to NaturalVegetation or similar
-@propagate_inbounds compute_albedo(i, j, grid, fields, veg::AbstractVegetation{NF}) where {NF} = veg.traits.albedo
+@inline compute_albedo(i, j, grid, fields, veg::AbstractVegetation{NF}) where {NF} = veg.traits.albedo
 
 # TODO: will need to change once PFTs are added
-@propagate_inbounds vegetation_area_fraction(i, j, grid, fields, ::Nothing) = zero(eltype(grid))
-@propagate_inbounds function vegetation_area_fraction(i, j, grid, fields, veg::AbstractVegetation)
-    if isnothing(veg.vegetation_dynamics)
-        LAI = fields.leaf_area_index[i, j, 1]
-        LAI_max = maximum_leaf_area_index(i, j, grid, fields, veg.traits)
-        f_veg = LAI / LAI_max
-        return f_veg
-    else
-        return vegetation_area_fraction(i, j, grid, fields, veg.vegetation_dynamics)
-    end
-end
+@inline vegetation_area_fraction(i, j, grid, fields, ::Union{Nothing, AbstractVegetation}) = zero(eltype(grid))
