9d3e366d1500b84fcc065ed862c0bd776f0036b7
Brian Groenke <brian.groenke@pik-potsdam.de>
2026-08-15 02:28:35 +0200

    Fix bugs in time utility methods

--- src/utils/time.jl
+++ src/utils/time.jl
@@ -17,20 +17,23 @@ Convert the given `time`stamp to a time delta relative to `reftime`.
 """
 timedelta(time::Union{Number, Period}) = timedelta(zero(time), time)
 timedelta(reftime, time) = timedelta(Second, reftime, time)
-timedelta(::Type{TT}, time::Union{Number, Period}) where {TT} = timedelta(TT, zero(time), time)
 timedelta(::Type{TT}, reftime::TimeType, time::TimeType) where {TT} = convert_dt(TT, time - reftime)
-timedelta(::Type{TT}, reftime::Number, time::Number) where {TT} = convert_dt(TT, time - reftime)
+# Fallback cases: the caller provided us with a Number or Period `time` which must already be a delta
+# If the reference time is also a delta, we treat it as an offset; otherwise, ignore it
+timedelta(::Type{TT}, Δt::Union{Number, Period}) where {TT} = timedelta(TT, zero(time), time)
+timedelta(::Type{TT}, reftime::Union{Number, Period}, Δt::Union{Number, Period}) where {TT} = convert_dt(TT, Δt) - convert_dt(TT, reftime)
+timedelta(::Type{TT}, reftime::TimeType, Δt::Union{Number, Period}) where {TT} = timestamp(TT, reftime, Δt)
 
 """
     $SIGNATURES
 
 Convert the time delta `Δt` to a suitable `timestamp` of type `TT` relative to `reftime`. If `TT` is a `TimeType`
-(i.e. `Date` or `DateTime`), then this method returns `reftime + Δt`. If `TT` is a `Period` or numeric type, `reftime`
-is ignored and the original `Δt` converted to the units implied by `TT` is returned.
+(i.e. `Date` or `DateTime`), then this method returns `reftime + Δt`.
 """
 timestamp(::Type{TT}, reftime::TimeType, Δt::Period) where {TT <: TimeType} = convert(TT, reftime + Δt)
 timestamp(::Type{TT}, reftime::TimeType, Δt::Number) where {TT <: TimeType} = convert(TT, reftime + convert_dt(Millisecond, Δt))
+# If the requested type TT is a delta (number or period), convert the given Δt
 timestamp(::Type{TT}, reftime::TimeType, Δt) where {TT <: Union{Period, Number}} = convert_dt(TT, Δt)
 timestamp(::Type{TT}, reftime::TimeType, Δt::TT) where {TT <: Union{Period, Number}} = Δt # if Δt type already matches, return as is
 timestamp(::Type{TT}, reftime::Number, Δt::Number) where {TT <: Union{Period, Number}} = convert_dt(TT, reftime + Δt)
-timestamp(::Type{TT}, reftime::TimeType, t::TimeType) where {TT <: Union{Period, Number}} = timedelta(TT, reftime, t)
+timestamp(::Type{TT}, reftime::TimeType, time::TimeType) where {TT <: Union{Period, Number}} = timedelta(TT, reftime, time)
